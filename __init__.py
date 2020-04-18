358b2b2052e0eeda905c7eb3617e5ff136195438
Francesca Sargent <francesca.sargent@admiralgroup.co.uk>
2020-04-18 20:22:58 +0100

    Add things that lewis smells like

--- __init__.py
+++ __init__.py
@@ -1,5 +1,29 @@
+import random
 from mycroft import MycroftSkill, intent_file_handler
 
+smells = {'smells': [
+    'Lush dirty spray',
+    'Beard oil',
+    'Hash browns',
+    'Milk chocolate',
+    'jose mourinho',
+    'solder',
+    'clocks',
+    'wet dog',
+    'palmolive soap',
+    'orange lucozade',
+    'original lucozade',
+    'his grey blanket',
+    'fried chicken',
+    'bed slobber',
+    'a lovely bit of squirrel',
+    'tangfastics',
+    'a really long sit down shower',
+    'lawnmower oil',
+    'hotel chocolat rocky road',
+    'the space bar after playing football manager for 3 hours',
+    'cheese wraps'
+]}
 
 class LewisSmells(MycroftSkill):
     def __init__(self):
@@ -7,7 +31,9 @@ class LewisSmells(MycroftSkill):
 
     @intent_file_handler('smells.lewis.intent')
     def handle_smells_lewis(self, message):
-        smell = ''
+        self.log.info("Seeing what lewis smells of.")
+        smell = random.choice(smells['smells'])
+        self.log.info(f"It's {smell}!")
 
         self.speak_dialog('smells.lewis', data={
             'smell': smell
